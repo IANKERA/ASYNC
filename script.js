bbb75a56bdc5c357a05a21abe5bfe2c5eaa44e0e
Your Name <giannhskeram@gmail.com>
2023-06-29 19:59:30 +0300

    event loop

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 const renderCountry = function (data) {
   const html = ` <article class="country">
-<img class="country__img" src="${data.flag}" />
+ <img class="country__img" src="${data.flag}" />
 <div class="country__data">
   <h3 class="country__name">${data.name}</h3>
   <h4 class="country__region">${data.region}</h4>
@@ -20,27 +20,37 @@ const renderCountry = function (data) {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryData = function (country) {
-  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
-    .then(response => response.json())
-    .then(data => {
-      renderCountry(data[0]);
-      const neighbour = data[0].borders?.[0];
+// const getCountryData = function (country) {
+//   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
 
-      if (!neighbour) return;
+//       if (!neighbour) return;
 
-      return fetch(
-        `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
-      );
-    })
-    .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'))
+//       return fetch(
+//         `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+//       );
+//     })
+//     .then(response => response.json())
+//     .then(data => renderCountry(data, 'neighbour'));
+// };
+// btn.addEventListener('click', function () {
+//   getCountryData('greece');
+// });
 
-  };
-  btn.addEventListener('click', function(){
-    getCountryData('greece')
+// const reverse = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(response => {
+//       if (!response.ok) throw new Error(`Probem ${response.status}`);
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data[0]));
+// };
+// reverse(39.71454, 21.62164);
 
-  })
-  
-  
-  
+console.log('test start');
+setTimeout(() => console.log('0 sec'), 0);
+Promise.resolve('resolved').then(res => console.log(res));
+console.log('Test end');
